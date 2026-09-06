04d0d7abf0ee9ac39fc5b309d4582a7b84e78e59
Dio R <diioradhitya@users.noreply.github.com>
2026-09-06 18:04:17 +0700

    fix: JavtifulProvider status 1 -> 2 (working)

--- JavtifulProvider/build.gradle.kts
+++ JavtifulProvider/build.gradle.kts
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1
+    status  = 2
     tvTypes = listOf("NSFW")
     iconUrl = "https://javtiful.com/uploads/site/icon/2026/04/27/new_jt_icon.png"
 }
